cb7555b28d4a0b700a312fd55f1ef4dd4910e648
Mihancheggg <schutow.mihail@yandex.ru>
2022-04-03 20:58:44 +0300

    19

--- Codewars.js
+++ Codewars.js
@@ -162,4 +162,13 @@ var greet = function(name) {
   return ("Hello " + name[0].toUpperCase() + name.substring(1).toLowerCase() + "!");
 };
 
-//№19 (7 kyu)
+//№19
+function positiveSum(arr) {
+  var sum = 0;
+  for (var i = 0; i <= arr.length; i++) {
+    arr[i] > 0 ? sum += arr[i] : sum +=0;
+  };
+  return sum
+}
+
+//№20 (7 kyu)
