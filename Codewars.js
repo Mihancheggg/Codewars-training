c908f220b86e000f5a10d7782f674cbd5d08f380
Mihancheggg <schutow.mihail@yandex.ru>
2022-04-04 20:41:10 +0300

    22

--- Codewars.js
+++ Codewars.js
@@ -192,3 +192,13 @@ var isSquare = function(n){
 }
 
 //№22 (7 kyu)
+function nbYear(p0, percent, aug, p) {
+  let years = 0;
+  while (p0 < p){
+    p0 = Math.floor(p0 + (p0 * percent / 100) + aug);      
+    years++
+  };
+return years;
+}
+
+//№23 (7 kyu)
